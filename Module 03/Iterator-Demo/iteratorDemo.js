76a036b5a918835f7cc16a3cb8fab8203488c957
aishsuja <74444364+aishsuja@users.noreply.github.com>
2020-11-19 17:38:22 +0530

    Add files via upload

---
+++ Module 03/Iterator-Demo/iteratorDemo.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+export default class IteratorDemo extends LightningElement {}
+contact = [
+    {
+        id: 'C1',
+        Name: 'Amy Taylor',
+        Email: 'ataylor@gmail.com',
+    },
+    {
+        id: 'C2',
+        Name: 'Michael Jones',
+        Email:'mjones@gmail.com',
+    },
+    {
+        id: 'C3',
+        Name: 'Jennifer Wu',
+        Email: 'jwu@gmail.com',
+    },
+];
